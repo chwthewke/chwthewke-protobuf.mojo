eef124e896b0ec69999fdef66d8cd7cf33541556
chewie <chewie@localhost>
2011-05-02 18:02:47 +0200

    [maven-protobuf-plugin] * fix: execution in an aribtrary cwd * (wip) source dependencies

--- src/main/java/net/chwthewke/maven/protobuf/ProtocMojo.java
+++ src/main/java/net/chwthewke/maven/protobuf/ProtocMojo.java
@@ -20,6 +20,13 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.factory.ArtifactFactory;
+import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.artifact.resolver.ArtifactNotFoundException;
+import org.apache.maven.artifact.resolver.ArtifactResolutionException;
+import org.apache.maven.artifact.resolver.ArtifactResolver;
+import org.apache.maven.artifact.versioning.VersionRange;
 import org.apache.maven.model.Dependency;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -36,7 +43,7 @@ import org.codehaus.plexus.util.cli.CommandLineException;
 import org.codehaus.plexus.util.cli.CommandLineUtils;
 import org.codehaus.plexus.util.cli.Commandline;
 import org.codehaus.plexus.util.cli.Commandline.Argument;
-import org.codehaus.plexus.util.cli.DefaultConsumer;
+import org.codehaus.plexus.util.cli.StreamConsumer;
 
 import com.google.common.base.Joiner;
 import com.google.common.collect.ImmutableList;
@@ -51,8 +58,6 @@ import com.google.common.collect.ImmutableList;
 public class ProtocMojo
         extends AbstractMojo
 {
-
-    private static final String PROTOCOL_ARCHIVE_ERROR = "Unable to create protocol archive.";
 
     /**
      * The source directories of the protocol.
@@ -73,6 +78,15 @@ public class ProtocMojo
      * @parameter
      */
     private Dependency[ ] protocolDependencies;
+
+    /**
+     * Location of the local repository.
+     * 
+     * @parameter expression="${localRepository}"
+     * @readonly
+     * @required
+     */
+    private ArtifactRepository localRepository;
 
     /**
      * The Maven project to analyze.
@@ -97,23 +111,26 @@ public class ProtocMojo
      */
     private MavenProjectHelper projectHelper;
 
-    private List<String> sourceDirs;
+    /**
+     * @component
+     * @readonly
+     */
+    private ArtifactFactory artifactFactory;
 
-    private List<String> sources;
+    /**
+     * @component
+     * @readonly
+     */
+    private ArtifactResolver artifactResolver;
 
-    private List<ProtocPlugin> protocPluginsList;
-
-    private Commandline commandline;
-
-    private File protoArchiveFile;
-
-    public void execute( )
-            throws MojoExecutionException
+    @Override
+    public void execute( ) throws MojoExecutionException
     {
-
         selectSourceDirectories( );
 
         selectSources( );
+
+        prepareProtocolDependencies( );
 
         selectProtocPlugins( );
 
@@ -122,9 +139,94 @@ public class ProtocMojo
         executeProtoc( );
     }
 
+    private void selectSourceDirectories( ) {
+        sourceDirs = sourceDirectories == null ?
+                ImmutableList.of( joinPaths( "src", "main", "proto" ) ) :
+                ImmutableList.copyOf( sourceDirectories );
+
+        getLog( ).debug( String.format( "Source directories: %s", sourceDirs ) );
+    }
+
+    private void selectSources( ) {
+        // TODO sources from artifacts
+        final FileSetManager fileSetManager = new FileSetManager( getLog( ) );
+
+        final FileSet fs = new FileSet( );
+        fs.setDirectory( project.getBasedir( ).getPath( ) );
+        for ( final String sourceDir : sourceDirs )
+            fs.addInclude( joinPaths( sourceDir, "**" ) );
+
+        sources = ImmutableList.copyOf( fileSetManager.getIncludedFiles( fs ) );
+
+        getLog( ).debug( String.format( "Source files: %s", sources ) );
+    }
+
+    private void prepareProtocolDependencies( ) throws MojoExecutionException {
+        if ( protocolDependencies == null )
+            return;
+        for ( final Dependency protocolDependency : protocolDependencies )
+            prepareProtocolDependency( protocolDependency );
+    }
+
+    private void prepareProtocolDependency( Dependency dependency ) throws MojoExecutionException {
+
+        final Artifact artifact = artifactFactory.createDependencyArtifact(
+            dependency.getGroupId( ),
+            dependency.getArtifactId( ),
+            VersionRange.createFromVersion( dependency.getVersion( ) ),
+            dependency.getType( ),
+            dependency.getClassifier( ),
+            Artifact.SCOPE_COMPILE );
+
+        try
+        {
+            artifactResolver.resolve( artifact,
+                project.getRemoteArtifactRepositories( ),
+                localRepository );
+        }
+        catch ( final ArtifactResolutionException e )
+        {
+            throw new MojoExecutionException( String.format( "Unable to resolve artifact: %s", artifact ), e );
+        }
+        catch ( final ArtifactNotFoundException e )
+        {
+            throw new MojoExecutionException( String.format( "Unable to find artifact: %s", artifact ), e );
+        }
+
+        //UnArchiver unarchiver = archiverManager.getUnArchiver( protocolDependency.getType( ) );
+
+        // TODO Auto-generated method stub
+
+    }
+
+    private void selectProtocPlugins( ) {
+
+        if ( protocPlugins == null )
+            protocPluginsList = ImmutableList.of(
+                new ProtocPlugin( "java", joinPaths( "target", "protobuf", "generated-sources", "java" ), true ) );
+        else
+            protocPluginsList = ImmutableList.copyOf( protocPlugins );
+
+        getLog( ).debug( String.format( "Requested plugins: %s", protocPluginsList ) );
+    }
+
+    private void prepareOutputDirectories( ) throws MojoExecutionException {
+        for ( final ProtocPlugin protocPlugin : protocPluginsList )
+        {
+            final String outputDirectory = protocPlugin.getOutputDirectory( );
+
+            final File file = new File( project.getBasedir( ), outputDirectory );
+            if ( file.isDirectory( ) )
+                continue;
+            if ( !file.mkdirs( ) )
+                throw new MojoExecutionException(
+                    String.format( "Error creating output directory %s.", file.getAbsolutePath( ) ) );
+        }
+    }
+
     private void executeProtoc( ) throws MojoExecutionException {
 
-        commandline = new Commandline( );
+        initCommandline( );
 
         computeCommand( );
 
@@ -137,60 +239,9 @@ public class ProtocMojo
         archiveAndAttachProtocolSources( );
     }
 
-    private void archiveAndAttachProtocolSources( ) throws MojoExecutionException {
-        archiveProtocolSources( );
-        attachProtoSourcesArtifact( );
-    }
-
-    private void attachProtoSourcesArtifact( ) {
-        projectHelper.attachArtifact( project, "jar", "proto", protoArchiveFile );
-    }
-
-    private void archiveProtocolSources( ) throws MojoExecutionException {
-        protoArchiveFile = new File( joinPaths( "target", "proto", "protocol-sources.jar" ) );
-        getLog( ).debug( String.format( "Archiving protocol sources to %s.", protoArchiveFile ) );
-        try
-        {
-            final Archiver archiver = archiverManager.getArchiver( "jar" );
-            archiver.setDestFile( protoArchiveFile );
-            for ( final String source : sources )
-                archiver.addFile( new File( source ), source );
-            archiver.createArchive( );
-        }
-        catch ( final NoSuchArchiverException e )
-        {
-            throw new MojoExecutionException( PROTOCOL_ARCHIVE_ERROR, e );
-        }
-        catch ( final ArchiverException e )
-        {
-            throw new MojoExecutionException( PROTOCOL_ARCHIVE_ERROR, e );
-        }
-        catch ( final IOException e )
-        {
-            throw new MojoExecutionException( PROTOCOL_ARCHIVE_ERROR, e );
-        }
-    }
-
-    private void addGeneratedSourcesToBuild( ) {
-        for ( final ProtocPlugin plugin : protocPluginsList )
-        {
-            if ( plugin.addToSources( ) )
-                project.addCompileSourceRoot( plugin.getOutputDirectory( ) );
-        }
-    }
-
-    private void runProtocSubprocess( ) throws MojoExecutionException {
-        try
-        {
-            getLog( ).debug( "Running protoc" );
-            final int exitValue =
-                    CommandLineUtils.executeCommandLine( commandline, new DefaultConsumer( ), new DefaultConsumer( ) );
-            getLog( ).debug( String.format( "Protoc exited with %d.", exitValue ) );
-        }
-        catch ( final CommandLineException e )
-        {
-            throw new MojoExecutionException( "Error creating protoc process", e );
-        }
+    private void initCommandline( ) {
+        commandline = new Commandline( );
+        commandline.setWorkingDirectory( project.getBasedir( ) );
     }
 
     private void computeCommand( ) {
@@ -212,23 +263,9 @@ public class ProtocMojo
 
     }
 
-    private void addSourcesToCommandLine( ) {
-        for ( final String source : sources )
-            addArgument( source );
-    }
-
-    private void addSourceDirsToCommandLine( ) {
-        for ( final String sourceDir : sourceDirs )
-            addSourceDirToCommandLine( sourceDir );
-    }
-
     private void addPluginsToCommandLine( ) {
         for ( final ProtocPlugin protocPlugin : protocPluginsList )
             addPluginToCommandLine( protocPlugin );
-    }
-
-    private void addSourceDirToCommandLine( final String sourceDir ) {
-        addArgument( String.format( "-I%s", sourceDir ) );
     }
 
     private void addPluginToCommandLine( final ProtocPlugin protocPlugin ) {
@@ -239,61 +276,118 @@ public class ProtocMojo
         // TODO Non standard plugins
     }
 
+    private void addSourceDirsToCommandLine( ) {
+        for ( final String sourceDir : sourceDirs )
+            addSourceDirToCommandLine( sourceDir );
+    }
+
+    private void addSourceDirToCommandLine( final String sourceDir ) {
+        addArgument( String.format( "-I%s", sourceDir ) );
+    }
+
+    private void addSourcesToCommandLine( ) {
+        for ( final String source : sources )
+            addArgument( source );
+    }
+
     private void addArgument( final String value ) {
         final Arg argument = new Argument( );
         argument.setValue( value );
         commandline.addArg( argument );
     }
 
-    private void prepareOutputDirectories( ) throws MojoExecutionException {
-        for ( final ProtocPlugin protocPlugin : protocPluginsList )
+    private void runProtocSubprocess( ) throws MojoExecutionException {
+        try
         {
-            final String outputDirectory = protocPlugin.getOutputDirectory( );
+            getLog( ).debug( "Running protoc" );
+            final int exitValue =
+                    CommandLineUtils.executeCommandLine( commandline, infoStreamConsumer, errorStreamConsumer );
 
-            final File file = new File( outputDirectory );
-            if ( file.isDirectory( ) )
-                continue;
-            if ( !file.mkdirs( ) )
-                throw new MojoExecutionException(
-                    String.format( "Error creating output directory %s.", file.getAbsolutePath( ) ) );
+            if ( exitValue != 0 )
+                throw new MojoExecutionException( String.format( "protoc encountered an error (%d)", exitValue ) );
+
+            getLog( ).info( "protoc executed successfully." );
+        }
+        catch ( final CommandLineException e )
+        {
+            throw new MojoExecutionException( "Error creating protoc process", e );
         }
     }
 
-    private void selectProtocPlugins( ) {
-
-        if ( protocPlugins == null )
-            protocPluginsList = ImmutableList.of(
-                new ProtocPlugin( "java", joinPaths( "target", "generated-sources", "java" ), true ) );
-        else
-            protocPluginsList = ImmutableList.copyOf( protocPlugins );
-
-        getLog( ).debug( String.format( "Requested plugins: %s", protocPluginsList ) );
+    private void archiveAndAttachProtocolSources( ) throws MojoExecutionException {
+        archiveProtocolSources( );
+        attachProtoSourcesArtifact( );
     }
 
-    private void selectSources( ) {
-        // TODO sources from artifacts
-        final FileSetManager fileSetManager = new FileSetManager( getLog( ) );
-
-        final FileSet fs = new FileSet( );
-        fs.setDirectory( project.getBasedir( ).getPath( ) );
-        for ( final String sourceDir : sourceDirs )
-            fs.addInclude( joinPaths( sourceDir, "**" ) );
-
-        sources = ImmutableList.copyOf( fileSetManager.getIncludedFiles( fs ) );
-
-        getLog( ).debug( String.format( "Source files: %s", sources ) );
+    private void attachProtoSourcesArtifact( ) {
+        projectHelper.attachArtifact( project, "jar", "proto", protoArchiveFile );
     }
 
-    private void selectSourceDirectories( ) {
-        sourceDirs = sourceDirectories == null ?
-                ImmutableList.of( joinPaths( "src", "main", "proto" ) ) :
-                ImmutableList.copyOf( sourceDirectories );
+    private void archiveProtocolSources( ) throws MojoExecutionException {
+        // TODO make archive name unique
+        protoArchiveFile = new File( project.getBasedir( ), joinPaths( "target", "protobuf", "protocol-sources.jar" ) );
+        getLog( ).debug( String.format( "Archiving protocol sources to %s.", protoArchiveFile ) );
 
-        getLog( ).debug( String.format( "Source directories: %s", sourceDirs ) );
+        Exception archiveException = null;
+        try
+        {
+            final Archiver archiver = archiverManager.getArchiver( "jar" );
+            archiver.setDestFile( protoArchiveFile );
+            for ( final String source : sources )
+                archiver.addFile( new File( project.getBasedir( ), source ), source );
+            archiver.createArchive( );
+        }
+        catch ( final NoSuchArchiverException e )
+        {
+            archiveException = e;
+        }
+        catch ( final ArchiverException e )
+        {
+            archiveException = e;
+        }
+        catch ( final IOException e )
+        {
+            archiveException = e;
+        }
+        if ( archiveException != null )
+        {
+            getLog( ).error( archiveException.toString( ) );
+            throw new MojoExecutionException( PROTOCOL_ARCHIVE_ERROR, archiveException );
+        }
+    }
+
+    private void addGeneratedSourcesToBuild( ) {
+        for ( final ProtocPlugin plugin : protocPluginsList )
+        {
+            if ( plugin.addToSources( ) )
+                project.addCompileSourceRoot( plugin.getOutputDirectory( ) );
+        }
     }
 
     private static String joinPaths( final String... elements ) {
         return Joiner.on( File.separator ).join( elements );
     }
+
+    private List<String> sourceDirs;
+    private List<String> sources;
+    private List<ProtocPlugin> protocPluginsList;
+    private Commandline commandline;
+    private File protoArchiveFile;
+
+    private final StreamConsumer infoStreamConsumer = new StreamConsumer( ) {
+        @Override
+        public void consumeLine( String line ) {
+            getLog( ).info( "[PROTOC]: " + line );
+        }
+    };
+
+    private final StreamConsumer errorStreamConsumer = new StreamConsumer( ) {
+        @Override
+        public void consumeLine( String line ) {
+            getLog( ).error( "[PROTOC]: " + line );
+        }
+    };
+
+    private static final String PROTOCOL_ARCHIVE_ERROR = "Unable to create protocol archive.";
 
 }
