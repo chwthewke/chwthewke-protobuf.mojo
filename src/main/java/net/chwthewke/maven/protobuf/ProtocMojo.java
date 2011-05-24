cb00c001aebcb9445d53f5a8422900c1ce0982d9
chewie <chewie@localhost>
2011-05-24 06:55:49 +0200

    [maven-protobuf-plugin] * fix license conflict * allow additional protoPaths

--- src/main/java/net/chwthewke/maven/protobuf/ProtocMojo.java
+++ src/main/java/net/chwthewke/maven/protobuf/ProtocMojo.java
@@ -1,21 +1,5 @@
 package net.chwthewke.maven.protobuf;
 
-/*
- * Copyright 2011 Thomas Dufour.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import static com.google.common.collect.Lists.newArrayList;
 
 import java.io.File;
@@ -76,6 +60,11 @@ public class ProtocMojo
      * @parameter
      */
     private String[ ] sourceDirectories;
+
+    /**
+     * @parameter
+     */
+    private String[ ] protoPaths;
 
     /**
      * The plugins to execute with their respective output directories.
@@ -510,6 +499,9 @@ public class ProtocMojo
     private void addSourceDirsToCommandLine( ) {
         for ( final String sourceDir : sourceDirectoriesList )
             addSourceDirToCommandLine( sourceDir );
+        if ( protoPaths != null )
+            for ( final String protoPath : protoPaths )
+                addSourceDirToCommandLine( PathUtils.fixPath( protoPath ) );
     }
 
     private void addSourceDirToCommandLine( final String sourceDir ) {
