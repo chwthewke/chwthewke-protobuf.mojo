444fc9191e682aff1bdf489691ef1c0f3e105218
Thomas Dufour <tdufour@ankama.com>
2013-09-13 16:19:24 +0200

    [protobuf-maven-plugin] File which was hidden by rename in previous commit

---
+++ src/main/java/net/chwthewke/maven/protobuf/source/DependencyProtocolSource.java
@@ -0,0 +1,64 @@
+package net.chwthewke.maven.protobuf.source;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import net.chwthewke.maven.protobuf.services.ServiceProvider;
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.model.Dependency;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.codehaus.plexus.util.FileUtils;
+
+import com.google.common.base.Function;
+
+public class DependencyProtocolSource extends AbstractDependencyProtocolSource {
+
+    public DependencyProtocolSource( ServiceProvider serviceProvider, Dependency dependency, boolean compileSources ) {
+        super( serviceProvider, dependency, compileSources );
+    }
+
+    @Override
+    public boolean collectChanges( ) throws MojoExecutionException {
+        serviceProvider.getLog( )
+            .info( String.format( "Resolving protocol dependency %s, compile sources: %s",
+                dependency, compileSources ) );
+
+        boolean hasChanged = false;
+
+        if ( compileSources )
+            hasChanged |= extractDependency( sourcePath( ) );
+        hasChanged |= extractDependency( includesPath( ) );
+
+        return hasChanged;
+    }
+
+    private boolean extractDependency( final Path targetPath )
+            throws MojoExecutionException {
+
+        final Artifact artifact = serviceProvider.getDependencyResolver( )
+            .resolveDependency( dependency, new Function<Artifact, Path>( ) {
+                @Override
+                public Path apply( final Artifact input ) {
+                    return input.getFile( ).toPath( ).resolve( "target" ).resolve( "classes" );
+                }
+            } );
+
+        if ( Files.isDirectory( artifact.getFile( ).toPath( ) ) )
+        {
+            try
+            {
+                FileUtils.copyDirectory( artifact.getFile( ), targetPath.toFile( ), "**/*.proto", "" );
+            }
+            catch ( IOException e )
+            {
+                throw new MojoExecutionException( String.format( "Could not copy build output for %s.", artifact ), e );
+            }
+
+        }
+
+        return serviceProvider.getArtifactExtractor( ).extractArtifact( artifact, targetPath );
+    }
+
+}
